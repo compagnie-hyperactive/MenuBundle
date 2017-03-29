a68993e2aa72db8969718ebf37f90d84d5cd49ee
Nicolas BONNIOT <nicolas@devgiants.fr>
2017-03-29 16:48:16 +0200

    Start Menu handling

---
+++ Entity/Menu.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Lch\MenuBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Menu
+ *
+ * @ORM\Table(name="menu")
+ * @ORM\Entity(repositoryClass="Lch\MenuBundle\Repository\MenuRepository")
+ */
+class Menu
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
